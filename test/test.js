c2e05aec97b583e0aadf8ad91c3e81a3bd028d46
Colum Foskin <columfoskin@gmail.com>
2016-09-21 21:04:34 +0100

    kjbafje

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('should respond with: Welcome to CICD teest node app!', function(done) {
+  it('should respond with: Welcome to CICD test node app!', function(done) {
 
     request(app).get('/').expect('Welcome the to CICD test node app!', done);
   });

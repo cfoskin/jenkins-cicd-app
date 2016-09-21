93fc7cc0f66e798a3a1171e45a66c41f0765841a
Colum Foskin <columfoskin@gmail.com>
2016-09-21 20:40:56 +0100

    push 2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
  
 app.get('/', function (req, res) {
   res.send('Welcome to CICD test bihgih node app!');
+  console.log("push 2");
 });
  
 app.listen(process.env.PORT || 3000);

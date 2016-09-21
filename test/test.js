f9bb7dabec01918dc69624644461e1cad4da2910
Colum Foskin <columfoskin@gmail.com>
2016-09-21 20:55:11 +0100

    commit 1

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
-f3jk
-    request(app).get('/').expect('Welcome the to CICD test nfewjlode apfe3ohi34p!', done);
+
+    request(app).get('/').expect('Welcome the to CICD test node app!', done);
   });
 });

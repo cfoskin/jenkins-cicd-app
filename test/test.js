b28566f135f2d74f101ebfbef5fb59c726bfbc45
Colum Foskin <columfoskin@gmail.com>
2016-09-21 19:46:32 +0100

    testing..4

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-	  	  this.timeout(500000);
+	  	  this.timeout(500);
 
   it('should respond with: Welcome to CICD test node app!', function(done) {
 

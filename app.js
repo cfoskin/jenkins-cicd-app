3195422274afcd976a98ed5732dbeab828b46fcd
Colum Foskin <columfoskin@gmail.com>
2016-09-21 19:49:44 +0100

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Welcome to CICD test node app!');
+  res.send('Welcome to CICD test bihgih node app!');
 });
  
 app.listen(process.env.PORT || 3000);

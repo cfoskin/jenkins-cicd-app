e150ac76f0fb080736e3f924868ef44e1f2b242d
Colum Foskin <columfoskin@gmail.com>
2016-09-22 11:45:18 +0100

    basic test should pass

--- test/test.js
+++ test/test.js
@@ -1,16 +1,9 @@
 var request = require('supertest');
 var app = require('../app.js');
-
-it('should give more info', function(done) {
-    this.timeout(10000);
-
-    setTimeout(function () {
-     describe('GET /', function() {
+ 
+describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome the to CICD test node app!', done);
-    console.log("test  567");
+    console.log("test 3");
   });
 });
-
-    }, 8000);
- });

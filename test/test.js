a73eae472b6dd277612e8c38d9234c131ee16fa8
Colum Foskin <columfoskin@gmail.com>
2016-09-22 21:10:57 +0100

    test1

--- test/test.js
+++ test/test.js
@@ -1,12 +1,6 @@
 var request = require('supertest');
 var app = require('../app.js');
-<<<<<<< HEAD
- 
 describe('GET /', function() {
-  it('should respond with: Welcome to  new branch  push 1 CICD test node app!', function(done) {
-=======
->>>>>>> master
-
 it('should give more info', function(done) {
     this.timeout(50000);
 
@@ -14,7 +8,7 @@ it('should give more info', function(done) {
      describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome the to CICD test node app!', done);
-    console.log("test 2");
+    console.log("test 1");
   });
 });
 

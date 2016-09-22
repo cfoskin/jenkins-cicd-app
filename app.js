08ed06f888a48656df062d5607f9da56db7048f5
Colum Foskin <columfoskin@gmail.com>
2016-09-22 10:48:34 +0100

    test 4 should pass

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Welcome the to CICD test node app the!');
+  res.send('Welcome the to CICD test node app!');
 });
  
 app.listen(process.env.PORT || 3000);

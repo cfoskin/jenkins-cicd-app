3eaef232c9050ec6fee13c2f90ee0997d18380ab
Colum Foskin <columfoskin@gmail.com>
2016-09-22 11:39:15 +0100

    test 1 should pass

--- test/test.js
+++ test/test.js
@@ -2,15 +2,15 @@ var request = require('supertest');
 var app = require('../app.js');
 
 it('should give more info', function(done) {
-    this.timeout(50000);
+    this.timeout(10000);
 
     setTimeout(function () {
      describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome the to CICD test node app!', done);
-    console.log("test 23");
+    console.log("test 1");
   });
 });
 
-    }, 25000);
+    }, 8000);
  });

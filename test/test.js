08ed509a740caae769ab2e34fa17784d1b1fb231
Colum Foskin <columfoskin@gmail.com>
2016-09-24 13:49:17 +0100

    test should pass no timeoj

--- test/test.js
+++ test/test.js
@@ -2,16 +2,23 @@
 var request = require('supertest');
 var app = require('../app.js');
 
-it('should give more info', function(done) {
-    this.timeout(30000);
+// it('should give more info', function(done) {
+//     this.timeout(30000);
 
-    setTimeout(function () {
-     describe('GET /', function() {
+//     setTimeout(function () {
+//      describe('GET /', function() {
+//   it('should respond with: Welcome to CICD test node app!', function(done) {
+//     request(app).get('/').expect('Welcome the to CICD test node app!', done);
+//     console.log("test 1");
+//   });
+// });
+
+//     }, 20000);
+//  });
+
+describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome the to CICD test node app!', done);
     console.log("test 1");
   });
 });
-
-    }, 20000);
- });

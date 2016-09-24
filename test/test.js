1b64ddc8b781499cc5821b50b6729781836032b7
Colum Foskin <columfoskin@gmail.com>
2016-09-24 14:27:05 +0100

    test should fail

--- test/test.js
+++ test/test.js
@@ -18,7 +18,8 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('should respond with: Welcome to CICD test node app!', function(done) {
-    request(app).get('/').expect('Welcome the to CICD test node app!', done);
-    console.log("test 1");
+    request(app).get('/').expect('Welcome the to CICD tfegrtest node app!', done);
+    console.log("test 1 passed");
   });
 });
+

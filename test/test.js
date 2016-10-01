00251e96c6f5419f87d90d2dec3ea61c8daae619
cfoskin <columfoskin@gmail.com>
2016-10-01 14:02:13 +0100

    test should pass

--- test/test.js
+++ test/test.js
@@ -19,7 +19,7 @@ var app = require('../app.js');
 describe('GET /', function() {
   it('should respond with: Welcome to the new CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome to the new CICD test node app!', done);
-  });
+  console.log('test1')});
 });
 
 

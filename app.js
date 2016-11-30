e53535cf528e4c9858361d19e4db698f5ea20ff5
Colum Foskin <columfoskin@gmail.com>
2016-11-30 11:36:01 +0000

    test should pass

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
- 
 var app = express();
- 
+
 app.get('/', function (req, res) {
   res.send('Welcome to the new CICD test node app!');
 });
- 
+
 app.listen(process.env.PORT || 3000);
- 
 module.exports = app;

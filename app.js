0edd2b0ead95647977737f61206b2d1ad03a279a
Colum Foskin <columfoskin@gmail.com>
2016-12-06 10:23:16 +0000

    should PASS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Welcome to the bad CICD test node app!');
+  res.send('Welcome to the CICD new test node app!');
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('should respond with: Welcome to the new CICD test node app!', function(done) {
-    request(app).get('/').expect('Welcome to the CICD test node app!', done);
+    request(app).get('/').expect('Welcome to the CICD new test node app!', done);
   console.log('test1')});
 });
 

68d5cf3b0df441e5fa0235e1d7610d466254e114
Colum Foskin <columfoskin@gmail.com>
2016-12-06 12:34:53 +0000

    should fail

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 var app = require('../app.js');
 
 describe('GET /', function() {
-  it('should respond with: Welcome to the CICD test node app!', function(done) {
+  it('should respond with: Welcome to the ghg CICD test node app!', function(done) {
     request(app).get('/').expect('Welcome to the CICD test node app!', done);
   console.log('test1')});
 });

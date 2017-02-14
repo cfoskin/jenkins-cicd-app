0ed6637fc4fdbd544ffe5eb6457a657c8ba96095
Colum Foskin <columfoskin@gmail.com>
2017-02-14 20:35:39 +0000

    test should pass

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var app = require('../app.js');
 describe('GET /', function() {
   it('Welcome to the CICD new test node app!', function(done) {
     request(app).get('/').expect('Welcome to the CICD new test node app!', done);
+ });
 });
 
 
